156a66cf5bbf3e9cf67b850b778b2a20d948cf3f
digitalknuckles <digitalknuckles@gmail.com>
2024-11-23 17:58:59 -0500

    initial commit from local.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
   // Select buttons and tab sections
   const buttons = document.querySelectorAll("menu button");
@@ -47,3 +48,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initialize the first tab on page load
   activateTab(0);
 });
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 14a3430 (initial commit from local.)
